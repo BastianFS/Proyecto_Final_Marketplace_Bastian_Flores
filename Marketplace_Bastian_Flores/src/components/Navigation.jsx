90bc60cb5c03c415c66cfd9e3e3f826f0baa51a4
Bastian <basti.flosan@gmail.com>
2025-02-06 01:24:50 -0300

    Style Navigation

--- Marketplace_Bastian_Flores/src/components/Navigation.jsx
+++ Marketplace_Bastian_Flores/src/components/Navigation.jsx
@@ -1,24 +1,27 @@
+import React from 'react';
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 import { Link } from 'react-router-dom';
+import FontAwesomeIcon from 'react-fontawesome'
+
 
 function Navigation() {
     return (
-        <Navbar expand="lg">
+        <Navbar expand="lg" className='bg-dark'>
             <Container className='container-fluid'>
                 <Navbar.Brand href="/" className='d-flex align-items-center'>
-                    <div className='flex-shrink-0 me-4' style={{ width: "5%" }}>
+                    <div className='pb-2 me-4' style={{ width: "15%" }}>
                         <img src="logo.svg" alt="Logo Solo Audio" className='img-fluid' />
                     </div>
                     <div>
-                        <h1 className='fst-italic fs-4'>Solo Audio</h1>
+                        <h1 className='fst-italic fs-4 text-white'>Solo Audio</h1>
                     </div>
                 </Navbar.Brand>
                 <Container className='container-fluid'>
@@ -38,14 +41,41 @@ function Navigation() {
                 </Container>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
+                    <Nav className="me-auto align-items-center">
+                        <Nav.Link className>
+                            <Link className="text-reset text-decoration-none" to="/">
+                                <Container className='d-flex align-items-center'>
+                                    <span style={{color: "grey"}}>
+                                        <i className="fa-solid fa-house fa-xl pe-1"></i>
+                                    </span>
+                                    <h2 className='text-white p-2'>Inicio</h2>
+                                </Container>
+                            </Link>
+                        </Nav.Link>
                         <Nav.Link>
-                            <Link className="text-reset text-decoration-none" to="/"><h2 className='text-secondary'>Inicio</h2></Link>
+                            <Link className="text-reset text-decoration-none d-flex" to="/">
+                                <Container className='d-flex align-items-center'>
+                                    <span style={{color: "grey"}}>
+                                        <i className="fa-solid fa-right-to-bracket pe-2 fa-2xl"></i>
+                                    </span>
+                                    <h4 className='text-white p-2'>Iniciar Sesión</h4>
+                                </Container>
+                            </Link>
+                        </Nav.Link>
+                        <Nav.Link>
+                            <Link className="text-reset text-decoration-none" to="/">
+                            <Container className='d-flex align-items-center'>
+                                    <span style={{color: "grey"}}>
+                                        <i className="fa-solid fa-user pe-2 fa-2xl"></i>
+                                    </span>
+                                    <h4 className='text-white p-2'>Registrarse</h4>
+                                </Container>
+                            </Link>
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
-        </Navbar>
+        </Navbar >
     );
 }
 
